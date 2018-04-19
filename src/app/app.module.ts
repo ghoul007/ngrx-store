b45b279bab44bc0d343146d6ce06eb473b03900e
ahmed <ghoul.ahmed5@gmail.com>
2018-04-19 23:56:22 +0100

    disptach user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Route, Routes} from  '@angular/router'
+import { RouterModule, Route, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { UserListComponent } from './user/user-list/user-list.component';
@@ -8,9 +8,12 @@ import { UserEditComponent } from './user/user-edit/user-edit.component';
 import { UserTableComponent } from './user/user-table/user-table.component';
 import { UserService } from './user/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from "@ngrx/store"
+import { userReducer } from './reducers/user.reducer';
 
-const routes:Routes = [{
-  path:"", component: UserListComponent
+
+const routes: Routes = [{
+  path: "", component: UserListComponent
 }]
 
 @NgModule({
@@ -23,7 +26,8 @@ const routes:Routes = [{
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    StoreModule.forRoot({ usrs: userReducer })
   ],
   providers: [UserService],
   bootstrap: [AppComponent]
--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { Observable } from 'rxjs/Observable';
 import { map } from "rxjs/operators/map";
+import { Store } from '@ngrx/store';
+import { AppState } from '../../models/app-state'
+import * as userActions from '../../actions/user.action';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
@@ -9,9 +12,13 @@ import { map } from "rxjs/operators/map";
 })
 export class UserListComponent implements OnInit {
 
-  users: Observable<Object>;
-  constructor(private userService: UserService) {
-    this.deleteUser = this.deleteUser.bind(this)
+  users$: Observable<Object>;
+  constructor(
+    private store: Store<AppState>
+    // private userService: UserService
+  ) {
+    this.deleteUser = this.deleteUser.bind(this);
+    this.users$ = this.store.select(state => state.users)
   }
 
   ngOnInit() {
@@ -20,15 +27,15 @@ export class UserListComponent implements OnInit {
 
 
   getUsers() {
-    this.users = this.userService.getUsers()
-   
+    // this.users$ = this.userService.getUsers()
+    this.store.dispatch(new userActions.LoadUserAction())
   }
 
 
   deleteUser(id) {
-    this.userService.deleteUsers(id).subscribe(res=>{
-      this.users = this.userService.getUsers().pipe(map(r=>Object(r).filter(f=>f['id']!==id)));
-      console.log(this.users.subscribe(console.log));
+    this.userService.deleteUsers(id).subscribe(res => {
+      this.users$ = this.userService.getUsers().pipe(map(r => Object(r).filter(f => f['id'] !== id)));
+      console.log(this.users$.subscribe(console.log));
     });
   }
 }
